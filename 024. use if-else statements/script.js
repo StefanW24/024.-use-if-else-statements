82fc4f160ba9c739ed1f8c66e0d5d8a9205a6a26
StefanW24 <90267126+StefanW24@users.noreply.github.com>
2021-11-29 16:34:28 +0100

    Add files via upload

---
+++ 024. use if-else statements/script.js
@@ -0,0 +1,34 @@
+const age = 18;
+
+if (age > 17) {
+
+    console.log("you are allowed in");
+
+} else {
+
+    console.log("you are too young and have to wait outside");
+
+}
+
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Welcome to the ladies party!")
+} else {
+
+    console.log("Sorry this party is for ladies only");
+
+}
+
+const driverstatus = "bob";
+
+if (driverstatus === "bob") {
+
+    console.log("You are allowed to drive");
+
+} else {
+
+    console.log("You are drunk. dont go behind the wheel");
+
+}
